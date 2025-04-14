31a925b92a343b699fd1a39c7f93fc2aa71195e0
Diwakar Jha <diwakarjha554@gmail.com>
2025-04-14 10:42:48 +0530

    project initiated and latest tailwind css and shadcn ui added

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import './globals.css';
+
+import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: 'Home - Stayhere',
+    template: '%s',
+  },
+  description:
+    'Vacation platform',
+  icons: {
+    icon: '/logo.webp',
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${poppins.className} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>stayhere</div>;
+};
+
+export default page;
