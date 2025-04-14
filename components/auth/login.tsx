3539bf22295260bfaeefc616c8ddbb4c544b6c6e
Diwakar Jha <diwakarjha554@gmail.com>
2025-04-14 15:16:04 +0530

    added login and signup

--- components/auth/login.tsx
+++ components/auth/login.tsx
@@ -5,7 +5,6 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import {
   Card,
   CardContent,
